55e968a0a98d9aead343fea3296e67139a5974ac
mathiofficial07 <153345044+mathiofficial07@users.noreply.github.com>
2026-06-30 09:51:45 +0530

    Delete frontend directory

--- frontend/kill_ts.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-del /s /q /f src\*.tsx src\*.ts
-del /q /f tsconfig.json tsconfig.app.json tsconfig.node.json vite.config.ts tailwind.config.ts vitest.config.ts playwright.config.ts playwright-fixture.ts src\vite-env.d.ts
-echo DONE
